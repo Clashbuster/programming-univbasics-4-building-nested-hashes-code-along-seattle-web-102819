2ea3a7eba4daba25e0687d04a7ffbf2c803c92b0
Taylor Coon <taylor.allan.coon@gmail.com>
2019-10-17 00:59:59 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,36 @@
 def first_challenge
-   epic_tragedy = {}
+ epic_tragedy = {
+  :montague => {
+    :patriarch => {
+      :name => "Lord Montague",
+      :age => "53"
+  },
+    :matriarch => {
+      :name => "Lady Montague",
+      :age => "54"
+  },
+    [{}, {}],
+    :matriarch => {
+      :name => "Lady Capulet",
+      :age => "51"
+    },
+    :heroine => {
+      :name => "Juliet",
+      :age => "15",
+      :status => "alive"
+    },
+    :heroine_friends=> [
+      {
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+      },
+      {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
+      }
+    ]
+  }
+}
 end
